2f7657b892be8fa4749997b58b21f6f398fe365a
Bamigboye Akinbode <53081200+bbamzi@users.noreply.github.com>
2022-11-30 10:32:57 +0100

    add an error handling validation for date range

--- socialmediadashboard/app/helper.py
+++ socialmediadashboard/app/helper.py
@@ -8,24 +8,27 @@ import time
 from django.utils import timezone
 import requests
 from .models import SocialDashboardData
+
+
 class SocialDashboardHelper:
     """This class gets and re-arrange data from api.social-searcher.com/v2"""
 
-    def get_tasks(self,session,keyword):
+    def get_tasks(self, session, keyword):
         """created as a placeholder for a future asynchronous loop"""
         tasks = []
-        networks = ['instagram', "twitter", "facebook", "youtube", "reddit", "flickr"]
+        networks = ['instagram', "twitter",
+                    "facebook", "youtube", "reddit", "flickr"]
         for net in networks:
             url = f'https://api.social-searcher.com/v2/search?q={keyword}&network={net}&limit=100&key={os.getenv("SOCIAL_MEDIA_SEARCH_API_KEY")}'
             tasks.append(asyncio.create_task(session.get(url, ssl=False)))
         return tasks
 
-    async def get_data(self,keyword):
+    async def get_data(self, keyword):
         """An asynchronous configuration for self.get_tasks() 
         returns a compiled list of data gotten from self.get_tasks """
         results = []
         async with aiohttp.ClientSession() as session:
-            tasks = self.get_tasks(session,keyword)
+            tasks = self.get_tasks(session, keyword)
             responses = await asyncio.gather(*tasks)
 
             for response in responses:
@@ -50,9 +53,9 @@ class SocialDashboardHelper:
             response = requests.get(url, headers=headers).json()
             return {"username": response['user']['username'], "profile_picture": response['user']['profile_pic_url']}
         except:
-            return {"username": 'N/A' ,"profile_picture": 'N/A'}
+            return {"username": 'N/A', "profile_picture": 'N/A'}
 
-    def get_username(self,network,obj):
+    def get_username(self, network, obj):
         if network == 'twitter' or network == "youtube" or network == 'reddit' or network == 'flickr':
             return obj.get('user', None).get('name', None)
         elif network == 'facebook':
@@ -84,24 +87,25 @@ class SocialDashboardHelper:
         if socials == "twitter":
             if obj.get('popularity', None):
                 engagement = {"retweet": obj.get('popularity', None)[0]['count'],
-                                  "likes": obj.get('popularity', None)[1]['count']}
+                              "likes": obj.get('popularity', None)[1]['count']}
                 return engagement
             else:
-                return {"retweet":0,"likes":0}
+                return {"retweet": 0, "likes": 0}
         elif socials == 'instagram':
             if obj.get('popularity', None):
                 engagement = {"comments": obj.get('popularity', None)[1]['count'],
-                                  "likes": obj.get('popularity', None)[0]['count']}
+                              "likes": obj.get('popularity', None)[0]['count']}
                 return engagement
             else:
-                return {"comment":0 , "likes":0}
+                return {"comment": 0, "likes": 0}
         else:
             return None
 
     def get_twitter_followers(self, social, obj):
         if social == 'twitter':
             try:
-                followers = obj.get('user', None).get('influence').get('count', None)
+                followers = obj.get('user', None).get(
+                    'influence').get('count', None)
                 return followers
             except AttributeError:
                 return None
@@ -112,57 +116,56 @@ class SocialDashboardHelper:
 
         if obj is not None:
             date_object = parse(obj[:-6])
-            return date_object
+            return timezone.make_aware(date_object)
         else:
             return timezone.now()
 
-
-    def gather_data(self,keyword):
+    def gather_data(self, keyword):
         items = []
-        check=[]
+        check = []
 
         for item in asyncio.run(self.get_data(keyword)):
             for obj in item['posts']:
-               try:
-                   items.append({"network":obj['network'],
-                                 "date_posted": self.format_date(obj.get('posted', None)),
-                                 "postid":obj['postid'],
-                                 "text_content":obj['text'],
-                                 "media_content":obj.get('image', None),
-                                 "language":obj.get('lang', None),
-                                 "sentiment": obj['sentiment'],
-                                 "post_url": obj['url'],
-                                 "profile_name":  obj.get('user', None).get('name', None) if  obj.get('user', None).get('name', None) else None,
-                                 "profile_picture":  obj.get('user', None).get('image', None) if  obj.get('user', None).get('image', None) else None,
-                                 "twitter_followers": self.get_twitter_followers(obj.get('network', None), obj),
-                                 "engagements": self.get_engagements(obj.get('network', None), obj)
-                   })
-               except KeyError:
-                   continue
+                try:
+                    items.append({"network": obj['network'],
+                                  "date_posted": self.format_date(obj.get('posted', None)),
+                                  "postid": obj['postid'],
+                                  "text_content": obj['text'],
+                                  "media_content": obj.get('image', None),
+                                  "language": obj.get('lang', None),
+                                  "sentiment": obj['sentiment'],
+                                  "post_url": obj['url'],
+                                  "profile_name":  obj.get('user', None).get('name', None) if obj.get('user', None).get('name', None) else None,
+                                  "profile_picture":  obj.get('user', None).get('image', None) if obj.get('user', None).get('image', None) else None,
+                                  "twitter_followers": self.get_twitter_followers(obj.get('network', None), obj),
+                                  "engagements": self.get_engagements(obj.get('network', None), obj)
+                                  })
+                except KeyError:
+                    continue
         return items
 
-    def save_data(self,keyword):
+    def save_data(self, keyword):
         """creates a new row if not in existence else update existing rows , where postid and newtork are 
         simultaneously equal"""
-        data = self.gather_data(keyword) 
+        data = self.gather_data(keyword)
         for item in data:
-            obj,created = SocialDashboardData.objects.update_or_create(
-                postid=item['postid'],network=item['network'],keyword=keyword,
+            obj, created = SocialDashboardData.objects.update_or_create(
+                postid=item['postid'], network=item['network'], keyword=keyword,
                 defaults={
-                    'keyword':keyword,
-                    'network':item['network'],
-                    "date_posted":item["date_posted"],
-                "text_content":item["text_content"],
-                "media_content":item["media_content"],
-               
-                "language":item["language"],
-                "sentiment":item["sentiment"],
-               
-                "post_url":item["post_url"],
-                "profile_name":item["profile_name"],
-                "profile_picture":item.get('profile_picture',None),
-                "twitter_followers":item["twitter_followers"],
-                "engagements":item["engagements"]
+                    'keyword': keyword,
+                    'network': item['network'],
+                    "date_posted": item["date_posted"],
+                    "text_content": item["text_content"],
+                    "media_content": item["media_content"],
+
+                    "language": item["language"],
+                    "sentiment": item["sentiment"],
+
+                    "post_url": item["post_url"],
+                    "profile_name": item["profile_name"],
+                    "profile_picture": item.get('profile_picture', None),
+                    "twitter_followers": item["twitter_followers"],
+                    "engagements": item["engagements"]
 
                 }
             )
--- socialmediadashboard/tests/test_views.py
+++ socialmediadashboard/tests/test_views.py
@@ -1,8 +1,8 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from rest_framework.test import (APITestCase)
 from rest_framework_api_key.models import APIKey
 
-from  django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework import status
 from django.urls import reverse
 
@@ -13,33 +13,40 @@ class SocialDashBoardTest(APITestCase):
         self.user = User.objects.create(email="test@test.com")
         self.user.set_password('1234')
         self.user.save()
-        self.url = reverse('get-social-media-dashboard' )
+        self.url = reverse('get-social-media-dashboard')
         api_key, key = APIKey.objects.create_key(name="my-remote-service")
         self.client.credentials(HTTP_AUTHORIZATION=f"Api-Key {key}")
 
     def test_for_missing_keyword(self):
-        response = self.client.get(path=self.url  + "?network=twitter")
+        response = self.client.get(path=self.url + "?network=twitter")
         print(response.json()['status'])
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json()['status'], False)
-        self.assertEqual(response.json()['Message'],  "You Need To Enter At least A Keyword")
+        self.assertEqual(
+            response.json()['Message'],  "You Need To Enter At least A Keyword")
 
     def test_for_added_keyword(self):
-        response = self.client.get(path=self.url  + "?keyword=testing&network=twitter")
+        response = self.client.get(
+            path=self.url + "?keyword=testing&network=twitter")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_get_social_media_filter_by_network(self):
-        response = self.client.get(path=self.url + "?keyword=testing&network=twitter")
+        response = self.client.get(
+            path=self.url + "?keyword=testing&network=twitter")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
     def test_get_social_media_filter_by_date_range(self):
-        response = self.client.get(path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05")
+        response = self.client.get(
+            path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
     def test_get_social_media_filter_by_multiple(self):
-        response = self.client.get(path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05&language=ar")
+        response = self.client.get(
+            path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05&language=ar")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
     def test_catch_error_on_invalid_filter(self):
-        response = self.client.get(path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05&city=greece") # city is the invalid filter here
+        # city is the invalid filter here
+        response = self.client.get(
+            path=self.url + "?keyword=testing&from_date=2022-10-05&to_date=2022-11-05&city=greece")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
